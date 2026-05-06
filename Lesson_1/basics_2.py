de1657fc0865c3cb24632a1e2faee2ab8f21b297
Viktor <viktor4strlsov@gmail.com>
2026-05-06 15:05:51 +0600

    Переменые типы данных

---
+++ Lesson_1/basics_2.py
@@ -0,0 +1,16 @@
+number = 5  # int Обявление переменой
+digit = 4.434  # float
+word = 'Результат: '  # string
+boolean = True  # boolean
+str_num = '5'
+
+print(number + int(str_num))  # Преобразование в число
+print(word + str(digit))  # Преобразование в страку
+print(word, digit, boolean)
+
+# del number  # Удаление переменой
+print(number)  # Вывод переменой если переменная удалена то будет ошибка
+
+number = 10  # Переназначене переменной
+
+print(number)  # Вывод переменой
