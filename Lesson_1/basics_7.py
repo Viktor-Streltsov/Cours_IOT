55fd18da04a641aff2a69c78e425893c2c417d2e
Viktor <viktor4strlsov@gmail.com>
2026-05-07 15:39:53 +0600

    Словари

---
+++ Lesson_1/basics_7.py
@@ -0,0 +1,8 @@
+# Словари
+# key может быть num, str, boolean, кортежем
+
+diction = {'key': 3}
+print(diction['key'])
+
+country = {'code': 'RU', 'name': 'Russian', 'population': 144}
+print(country['code'])
